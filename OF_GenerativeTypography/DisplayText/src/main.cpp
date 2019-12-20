7b9629089349bdad7223f0812359b9a71f03b1fa
Aris Bezas <igoumeninja@gmail.com>
2019-12-20 15:40:19 +0200

    kkk

--- OF_GenerativeTypography/DisplayText/src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "ofMain.h"
-#include "ofApp.h"
-
-//========================================================================
-int main(){
-	ofSetupOpenGL(640, 360, OF_WINDOW);
-	ofRunApp(new ofApp());
-}
